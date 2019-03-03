32c90bdd4e3cb57b892c5d7069ad14d89d8a1b4c
yun fu <y.fu@cs.ucl.ac.uk>
2019-03-03 12:03:46 +0000

    Update config.js

--- music_player_nodejs_mysql/config.js
+++ music_player_nodejs_mysql/config.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   port: 3000,
   user: 'root',
-  password: 'szfy2511',
+  password: 'design2019',
   database: 'music',
   viewPath: path.join(__dirname, 'views')
 }
