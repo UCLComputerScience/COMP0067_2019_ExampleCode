975947cd6c91dc759877fac2ff16d50f21425d6c
yun fu <y.fu@cs.ucl.ac.uk>
2019-03-03 15:17:49 +0000

    update

--- Nodejs_MySQL_Demo/app.js
+++
@@ -1,8 +0,0 @@
-  const db = require('./dbconnection');
-
-  let sql = `CREATE TABLE IF NOT EXISTS testing (
-              id INT AUTO_INCREMENT PRIMARY KEY);`;
-  db.query(sql, (err, result) => {
-          if (err) throw err;
-          console.log("Ran query");
-  });
