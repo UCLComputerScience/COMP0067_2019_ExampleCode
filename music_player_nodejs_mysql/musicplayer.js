3273073a271a65c20d3a0ddea1f0aa3c27d50361
yun fu <y.fu@cs.ucl.ac.uk>
2019-03-03 12:14:54 +0000

    Update musicplayer.js

--- music_player_nodejs_mysql/musicplayer.js
+++ music_player_nodejs_mysql/musicplayer.js
@@ -17,9 +17,9 @@ server.on('request', function(req, res) {
 // })
 
 var server = http.createServer(function (req, res) {
-	console.log('sever start');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+	// console.log('sever start');
+ //  res.writeHead(200, {'Content-Type': 'text/plain'});
+ //  res.end('Hello World!');
   
    
 })
