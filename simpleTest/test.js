018e89ec324c7ae90693b1ae2bbecb273ff9a5d3
yun fu <y.fu@cs.ucl.ac.uk>
2019-03-03 11:06:02 +0000

    add console output

--- simpleTest/test.js
+++ simpleTest/test.js
@@ -3,4 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
+  
+   console.log('server is listening at port 3000');
 }).listen(3000);
