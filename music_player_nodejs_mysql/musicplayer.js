ed71e529fd54763cd164e17365b7b2cfc5020d9a
yun fu <y.fu@cs.ucl.ac.uk>
2019-03-03 11:34:16 +0000

    Update musicplayer.js

--- music_player_nodejs_mysql/musicplayer.js
+++ music_player_nodejs_mysql/musicplayer.js
@@ -15,6 +15,13 @@ var server = http.createServer();
 //   console.log('pleast visit http://' + config.host + ':' + config.port);
 // })
 
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+  
+   console.log('sever start');
+})
+
 server.listen(3000, function() {
      console.log('server is listening at port 3000');
 })
