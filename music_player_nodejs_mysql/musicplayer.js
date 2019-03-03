d4406c1d4f8651eb715133bdda82773b025024f7
yun fu <y.fu@cs.ucl.ac.uk>
2019-03-03 11:21:38 +0000

    Update musicplayer.js

--- music_player_nodejs_mysql/musicplayer.js
+++ music_player_nodejs_mysql/musicplayer.js
@@ -1,18 +1,26 @@
-var http = require('http');
-var config = require('./config');
+// var http = require('http');
+// var config = require('./config');
 // var router = require('./router');
 // var render = require('./common/render');
 
-var server = http.createServer();
+// var server = http.createServer();
 
-server.on('request', function(req, res) {
-  // render(res);
-  // router(req, res);
-})
+// server.on('request', function(req, res) {
+//   render(res);
+//   router(req, res);
+// })
 
-server.listen(config.port, config.host, function() {
-  console.log('server is listening at port ' + config.port);
-  console.log('pleast visit http://' + config.host + ':' + config.port);
-})
+// server.listen(config.port, config.host, function() {
+//   console.log('server is listening at port ' + config.port);
+//   console.log('pleast visit http://' + config.host + ':' + config.port);
+// })
 
 
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+  
+   console.log('server is listening at port 3000');
+}).listen(3000);
